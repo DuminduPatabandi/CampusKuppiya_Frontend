fc31c16ade77b080704f4bb7e66097841fdde22b
DuminduPatabandi <dppatabandi@gmail.com>
2023-05-09 09:32:25 +0530

    initial commit to front end

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-    baseURL: "http://localhost:3000/api/",
+    baseURL: "https://blooming-basin-02556.herokuapp.com/",
     withCredentials: true
 })
+// const newRequest = axios.create({
+//     baseURL: "http://localhost:3000/api/",
+//     withCredentials: true
+// })
 
 export default newRequest
